1a48d8dd517940f8828d38758540eb86b41f6f7a
Zrahay <shubhangsinha44@gmail.com>
2026-07-14 17:48:50 +0530

    Changes in test files, registering the models in __init__ and updating .rst docs

--- deepchem/models/__init__.py
+++ deepchem/models/__init__.py
@@ -65,6 +65,7 @@ try:
     from deepchem.models.torch_models import Chemberta
     from deepchem.models.torch_models import MoLFormer
     from deepchem.models.torch_models import OneFormer
+    from deepchem.models.torch_models import Dnabert
 except ImportError as e:
     logger.warning(e)
 
